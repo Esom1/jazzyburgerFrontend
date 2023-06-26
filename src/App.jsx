b67e631354b7660f861ef95d91f9ea003d3af401
christy <christinaugoala@gmail.com>
2023-06-26 10:43:38 +0100

    modified navbar in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { BrowserRouter, Routes, Route, Router } from 'react-router-dom';
-import Navbar from './layout/navbar';
+import Navbar from './layout/Navbar';
 import './App.css'
 import Footer from './layout/footer';
 import Home from './pages/Home';
